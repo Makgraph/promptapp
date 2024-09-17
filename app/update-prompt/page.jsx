9b47e5a8036e208abad1a23375a1a46e138c2b4a
Makgraph <milienmakenson3@gmail.com>
2024-09-17 15:27:06 -0400

    add suspense to update-prompt

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,4 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
@@ -15,16 +13,18 @@ const UpdatePrompt = () => {
 
   useEffect(() => {
     const getPromptDetails = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`);
-      const data = await response.json();
+      if (promptId) {
+        const response = await fetch(`/api/prompt/${promptId}`);
+        const data = await response.json();
 
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
-      });
+        setPost({
+          prompt: data.prompt,
+          tag: data.tag,
+        });
+      }
     };
 
-    if (promptId) getPromptDetails();
+    getPromptDetails();
   }, [promptId]);
 
   const updatePrompt = async (e) => {
@@ -36,6 +36,9 @@ const UpdatePrompt = () => {
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
         method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify({
           prompt: post.prompt,
           tag: post.tag,
@@ -63,4 +66,77 @@ const UpdatePrompt = () => {
   );
 };
 
-export default UpdatePrompt;
+const Page = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <UpdatePrompt />
+  </Suspense>
+);
+
+export default Page;
+
+// "use client";
+
+// import { useEffect, useState } from "react";
+// import { useRouter, useSearchParams } from "next/navigation";
+
+// import Form from "@components/Form";
+
+// const UpdatePrompt = () => {
+//   const router = useRouter();
+//   const searchParams = useSearchParams();
+//   const promptId = searchParams.get("id");
+
+//   const [post, setPost] = useState({ prompt: "", tag: "" });
+//   const [submitting, setIsSubmitting] = useState(false);
+
+//   useEffect(() => {
+//     const getPromptDetails = async () => {
+//       const response = await fetch(`/api/prompt/${promptId}`);
+//       const data = await response.json();
+
+//       setPost({
+//         prompt: data.prompt,
+//         tag: data.tag,
+//       });
+//     };
+
+//     if (promptId) getPromptDetails();
+//   }, [promptId]);
+
+//   const updatePrompt = async (e) => {
+//     e.preventDefault();
+//     setIsSubmitting(true);
+
+//     if (!promptId) return alert("Missing PromptId!");
+
+//     try {
+//       const response = await fetch(`/api/prompt/${promptId}`, {
+//         method: "PATCH",
+//         body: JSON.stringify({
+//           prompt: post.prompt,
+//           tag: post.tag,
+//         }),
+//       });
+
+//       if (response.ok) {
+//         router.push("/");
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     } finally {
+//       setIsSubmitting(false);
+//     }
+//   };
+
+//   return (
+//     <Form
+//       type="Edit"
+//       post={post}
+//       setPost={setPost}
+//       submitting={submitting}
+//       handleSubmit={updatePrompt}
+//     />
+//   );
+// };
+
+// export default UpdatePrompt;
